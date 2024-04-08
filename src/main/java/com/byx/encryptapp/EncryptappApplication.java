140c7af5c59b82403563b3c7ae154473aaf44a43
nesralla <nesralla@gmail.com>
2024-04-08 13:51:10 -0300

    change namespace

--- src/main/java/com/byx/encryptapp/EncryptappApplication.java
+++ src/main/java/com/byx/encryptapp/EncryptappApplication.java
@@ -1,4 +1,4 @@
-package com.cip.encryptapp;
+package com.byx.encryptapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/byx/encryptapp/controllers/XmlController.java
+++ src/main/java/com/byx/encryptapp/controllers/XmlController.java
@@ -1,4 +1,4 @@
-package com.cip.encryptapp.controllers;
+package com.byx.encryptapp.controllers;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
